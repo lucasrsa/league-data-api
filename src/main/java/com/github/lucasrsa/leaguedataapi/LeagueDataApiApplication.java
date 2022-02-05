1c92d65ddf9a2f29cabb139e3f64b6c12cba2e6b
lucasrsa <lucasrsilva1604@gmail.com>
2022-02-05 10:11:17 -0300

    Initial commit

---
+++ src/main/java/com/github/lucasrsa/leaguedataapi/LeagueDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasrsa.leaguedataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasrsa/leaguedataapi/LeagueDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasrsa.leaguedataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
